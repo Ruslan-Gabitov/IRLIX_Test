c66464a4af994a6b630cfe8b4f85a6959db513a2
Ruslan Gabitov <rgabitov355@gmail.com>
2024-04-29 00:14:28 +0500

    feat: change the functions to run one of the two browsers

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,24 +10,41 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 
 
 @pytest.fixture(scope="session")
-def browser():
-    chrome_options = Options()
-    chrome_options.add_argument("--window-size=1980,1080")
-    chrome_options.add_argument("--log-level=3")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_argument("--headless")
+def browser(request):
+    if request.config.getoption("--firefox"):
+        firefox_options = Options_firefox()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--width=1980")
+        firefox_options.add_argument("--height=1080")
 
-    print("Открытие браузера")
-    driver = webdriver.Chrome(service=ChromiumService(
-        ChromeDriverManager().install()), options=chrome_options)
-    mp = MainPage(driver)
-    mp.go_to_site()
+        print("Открытие браузера Firefox")
+        driver = webdriver.Firefox(service=FirefoxService(
+            GeckoDriverManager().install()), options=firefox_options)
+        mp = MainPage(driver)
+        mp.go_to_site()
 
-    yield mp
+        yield mp
 
-    driver.quit()
-    print("Закрытие браузера")
+        driver.quit()
+        print("Закрытие браузера Firefox")
+    else:
+        chrome_options = Options_chrome()
+        chrome_options.add_argument("--window-size=1980,1080")
+        chrome_options.add_argument("--log-level=3")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument("--headless")
+
+        print("Открытие браузера Chrome")
+        driver = webdriver.Chrome(service=ChromiumService(
+            ChromeDriverManager().install()), options=chrome_options)
+        mp = MainPage(driver)
+        mp.go_to_site()
+
+        yield mp
+
+        driver.quit()
+        print("Закрытие браузера Chrome")
 
 
 def pytest_addoption(parser):
