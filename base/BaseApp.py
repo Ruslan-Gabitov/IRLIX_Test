c10b27c6dc751b5a78318a285704c540d1059110
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-01 21:42:32 +0500

    refactor: deleted import ActionChains

--- base/BaseApp.py
+++ base/BaseApp.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class Base:
