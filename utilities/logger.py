c4db207ecaa1b1461b58b82b31421b72fbec2db7
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-02 14:53:22 +0500

    refactor: adding comments to the code

--- utilities/logger.py
+++ utilities/logger.py
@@ -3,19 +3,28 @@ import os
 
 
 class Logger():
+
+    """Класс записи логирования тестов в файл"""
+
+    # Проверка существования директории для записи логов
     if not os.path.exists("logs"):
         os.makedirs("logs")
 
+    # Создание имени файла с текущим временем и датой
     file_name = fr"logs\log_{
         str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))}.log"
 
+    # Создание файла с расширением .log и последующая до запись в него логов
     @classmethod
     def write_log_to_file(cls, data: str):
         with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
             logger_file.write(data)
 
+    # Получение логов при старте тестовой функции
     @classmethod
     def add_start_step(cls, method: str):
+
+        # Получение наименования текущего теста из переменной окружения
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
         data_to_add = f"\n{'=' * 50}\n"
@@ -25,6 +34,7 @@ class Logger():
 
         cls.write_log_to_file(data_to_add)
 
+    # Получение логов при завершении работы тестовой функции
     @classmethod
     def add_end_step(cls, url: str, method: str):
 
