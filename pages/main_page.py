30cf7fe4546ffbc1d18aed900804f137dfcba059
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-02 12:43:47 +0500

    refactor: adding comments to the code

--- pages/main_page.py
+++ pages/main_page.py
@@ -18,6 +18,7 @@ class locators:
 
 class MainPage(Base):
     """Класс описывающий главную страницу сайта"""
+
     def __init__(self, driver):
         super().__init__(driver)
 
@@ -28,63 +29,64 @@ class MainPage(Base):
     """Actions"""
 
     def click_link_become_customer_header(self) -> None:
-        """Функция ожидает WebElement в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement в течении 10 сек и возвращает его
                     в случаи если он кликабелен и делает клик по этому элементу"""
         self.find_element_clickable(
             locators=locators.link_become_customer_header).click()
         print("> Клик по ссылке \"Стать клиентом\"")
 
     def input_name(self, name: str) -> None:
-        """Функция ожидает WebElement поля ИМЯ в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement поля ИМЯ в течении 10 сек и возвращает его
                     в случаи если он кликабелен и добавляет в него значение"""
         self.find_element_clickable(
             locators=locators.input_name).send_keys(name)
         print("> Добавлено имя в поле ИМЯ")
 
     def input_company(self, company_name: str) -> None:
-        """Функция ожидает WebElement поля КОМПАНИЯ в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement поля КОМПАНИЯ в течении 10 сек и возвращает его
                     в случаи если он кликабелен и добавляет в него значение"""
         self.find_element_clickable(
             locators=locators.input_company).send_keys(company_name)
         print("> Добавлено название компании в поле КОМПАНИЯ")
 
     def input_email(self, email: str) -> None:
-        """Функция ожидает WebElement поля ПОЧТА в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement поля ПОЧТА в течении 10 сек и возвращает его
                     в случаи если он кликабелен и добавляет в него значение"""
         self.find_element_clickable(
             locators=locators.input_email).send_keys(email)
         print("> Добавлен email в поле ПОЧТА")
 
     def input_phone(self, phone) -> None:
-        """Функция ожидает WebElement поля ТЕЛЕФОН в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement поля ТЕЛЕФОН в течении 10 сек и возвращает его
                     в случаи если он кликабелен и добавляет в него значение"""
         self.find_element_clickable(
             locators=locators.input_phone).send_keys(phone)
         print("> Добавлен телефон в поле ТЕЛЕФОН")
 
     def input_telegram(self, telegram_user) -> None:
-        """Функция ожидает WebElement поля ТЕЛЕГРАМ в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement поля ТЕЛЕГРАМ в течении 10 сек и возвращает его
                     в случаи если он кликабелен и добавляет в него значение"""
         self.find_element_clickable(
             locators=locators.input_telegram).send_keys(telegram_user)
         print("> Добавлен телеграм в поле ТЕЛЕГРАМ")
 
     def click_checkbox_qa(self) -> None:
-        """Функция ожидает WebElement (чек-бокс QA) в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement (чек-бокс QA) в течении 10 сек и возвращает его
                     в случаи если он присутствует в DOM и делает клик по чек-боксу"""
         self.find_element(locators=locators.input_checkbox_qa).click()
         print("> Клик по чек-боксу \"QA\"")
 
     def click_policy_checkbox(self) -> None:
-        """Функция ожидает WebElement (чек-бокс "Согласие на обработку данных") в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement (чек-бокс "Согласие на обработку данных") в течении 10 сек и возвращает его
                     в случаи если он присутствует в DOM и делает клик по чек-боксу"""
         self.find_element(locators=locators.policy_checkbox).click()
         print("> Клик по чек-боксу \"Согласие на обработку данных\"")
 
     def click_button_go_to_telegram(self):
-        """Функция ожидает WebElement (кнопку "Написать") в течении 10 сек и делает scroll до 
+        """Функция ожидает WebElement (кнопку "Написать") в течении 10 сек и делает scroll до
                     элемента в случаи если он кликабелен"""
-        self.move_to_clickable_element(locators=locators.button_go_to_telegram).click()
+        self.move_to_clickable_element(
+            locators=locators.button_go_to_telegram).click()
         print("> Клик по кнопке \"Написать\"")
 
 
