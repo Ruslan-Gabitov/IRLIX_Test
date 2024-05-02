63eb5da8587e44cc8a299c63db20d44e80714727
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-02 19:42:34 +0500

    refactor: rename fixture

--- tests/test_button_go_to_telegram.py
+++ tests/test_button_go_to_telegram.py
@@ -8,7 +8,7 @@ import allure
 class TestButtonGoToTelegram:
     """Класс функционального тестирования кнопки написать, что она кликабельна и ведет на сайт Telegram"""
 
-    def test_click_button_go_to_telegram(self, browser: Type[MainPage]):
+    def test_click_button_go_to_telegram(self, page_main: Type[MainPage]):
         """Функция тестирования кнопки "Написать" """
 
         # Шаг сбора отчета Allure
@@ -19,29 +19,29 @@ class TestButtonGoToTelegram:
                 method="test_click_link_become_customer_header")
 
             # Ожидание кликабельного WebElement-а и scroll до него и клик
-            browser.move_to_clickable_element(
+            page_main.move_to_clickable_element(
                 locators=locators.button_go_to_telegram).click()
 
             # Переход на вновь открывшеюся вкладку (в случаи с Chrome). В случаи FairFox
             # происходит перехват исключения NoSuchWindowException
-            browser.switch_to_window(1)
+            page_main.switch_to_window(1)
 
             # Создание и сохранение скриншота
-            browser.save_screenshot(path=r"TestButtonGoToTelegram")
+            page_main.save_screenshot(path=r"TestButtonGoToTelegram")
 
             # Проверка, что перешли на новую вкладку и там есть текст @irlix_it
-            assert browser.find_element(
+            assert page_main.find_element(
                 ".//div[@class='tgme_page_extra']").text.strip() == "@irlix_it"
 
             # Закрытие текущей вкладки браузера
-            browser.close()
+            page_main.close()
 
             # Возврат на начальную вкладку браузера
-            browser.switch_to_window(0)
+            page_main.switch_to_window(0)
 
             # Перезагрузка начально вкладки браузера для последующих тестов
-            browser.refresh()
+            page_main.refresh()
 
             # Конец записи логирования данной функции
             Logger.add_end_step(
-                method="test_click_link_become_customer_header", url=browser.current_url())
+                method="test_click_link_become_customer_header", url=page_main.current_url())
