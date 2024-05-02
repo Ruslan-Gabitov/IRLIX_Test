f278077c6e703948cf841ccc8cdba64b8178a7e0
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-02 12:15:12 +0500

    refactor: adding comments to the code

--- base/BaseApp.py
+++ base/BaseApp.py
@@ -9,23 +9,33 @@ from selenium.common.exceptions import NoSuchWindowException
 
 
 class Base:
+    """Базовый класс методов selenium от которого наследуется классы Page"""
     def __init__(self, driver):
         self.driver = driver
         self.base_url = "https://irlix.com/"
 
     def find_element_clickable(self, locators: str, time: int = 10) -> WebElement:
+        """Функция ожидает WebElement в течении 10 сек и возвращает его 
+                    в случаи если он кликабелен"""
         return WebDriverWait(self.driver, time).until(EC.element_to_be_clickable((By.XPATH, locators)),
                                                       message=f"Can't find element by locator {locators}")
 
     def find_element(self, locators: str, time: int = 10) -> WebElement:
+        """Функция ожидает WebElement в течении 10 сек и возвращает его 
+                    в случаи если он присутствует в DOM"""
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located((By.XPATH, locators)),
                                                       message=f"Can't find element by locator {locators}")
 
     def find_elements(self, locators: str, time: int = 10) -> list[WebElement]:
+        """Функция ожидает WebElement в течении 10 сек и возвращает список эелементов
+                    в случаи если он присутствует в DOM"""
         return WebDriverWait(self.driver, time).until(EC.presence_of_elements_located((By.XPATH, locators)),
                                                       message=f"Can't find element by locator {locators}")
 
     def switch_to_window(self, number_window: int) -> None:
+        """Функция для перехода на новую вкладку (работает корректно с Chrome),
+                 конструкция try/except необходима потому что в браузере Firefox при открытии новой вкладки,
+                она автоматически становится активной и на нее нет необходимости переключаться"""
         try:
             self.driver.switch_to.window(
                 self.driver.window_handles[number_window])
@@ -33,12 +43,17 @@ class Base:
             pass
 
     def move_to_clickable_element(self, locators: str):
+        """Функция проверяет присутствие кликабельного элемента на странице
+            и делает scroll до него. Использование execute_script необходимо
+            для корректной работы в двух браузерах Firefox и Chrome"""
         element = self.find_element_clickable(locators)
         self.driver.execute_script(
             "arguments[0].scrollIntoView(true);", element)
         return element
 
     def save_screenshot(self, path: str) -> None:
+        """Функция сначала проверяет существует ли директория, 
+        если нет то создает ее и сохраняет скриншот в нее, с указанием даты и времени. """
         if not os.path.exists("screen"):
             os.makedirs("screen")
         utc_tz = pytz.timezone('Europe/Moscow')
@@ -48,13 +63,17 @@ class Base:
             f"screen\{path}_{formatted_string}.png")
 
     def current_url(self) -> str:
+        """Функция получает URL текущей, активной страницы"""
         return self.driver.current_url
 
     def go_to_site(self):
+        """Функция открытия вкладки с базовым URL"""
         return self.driver.get(self.base_url)
 
     def close(self):
+        """Функция закрытия текущей активной вкладки"""
         self.driver.close()
 
     def refresh(self):
+        """Функция перезагрузки текущей активной вкладки"""
         self.driver.refresh()
