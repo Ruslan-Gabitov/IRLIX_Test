3d6f094431fb14fcb1486bb593f42e1ed3bb4ef6
Ruslan Gabitov <rgabitov355@gmail.com>
2024-05-15 13:38:14 +0500

    refactor: remove spaces

--- base/BaseApp.py
+++ base/BaseApp.py
@@ -17,35 +17,35 @@ class Base:
         self.base_url = "https://irlix.com/"
 
     def find_element_clickable(self, locator: str, time: int = 10) -> WebElement:
-        """Функция ожидает WebElement в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement в течении 10 сек и возвращает его
                     в случаи если он кликабелен"""
 
         with allure.step("[find_element_clickable] Получение кликабельного веб-элемента"):
             return WebDriverWait(self.driver, time).until(EC.element_to_be_clickable((By.XPATH, locator)),
-                                                        message=f"Can't find element by locator {locator}")
+                                                          message=f"Can't find element by locator {locator}")
 
     def find_element(self, locator: str, time: int = 10) -> WebElement:
-        """Функция ожидает WebElement в течении 10 сек и возвращает его 
+        """Функция ожидает WebElement в течении 10 сек и возвращает его
                     в случаи если он присутствует в DOM"""
-        
+
         with allure.step("[find_element] Получение веб-элемента который присутствует в DOM"):
             return WebDriverWait(self.driver, time).until(EC.presence_of_element_located((By.XPATH, locator)),
-                                                        message=f"Can't find element by locator {locator}")
+                                                          message=f"Can't find element by locator {locator}")
 
     def find_elements(self, locator: str, time: int = 10) -> list[WebElement]:
         """Функция ожидает WebElement в течении 10 сек и возвращает список элементов
                     в случаи если он присутствует в DOM"""
-        
+
         with allure.step("[find_elements] Получение списка веб-элемента которые присутствуют в DOM"):
             self.find_element(locator=locator)
-            
+
             return self.driver.find_elements(By.XPATH, locator)
 
     def switch_to_window(self, number_window: int) -> None:
         """Функция для перехода на новую вкладку (работает корректно с Chrome),
                  конструкция try/except необходима потому что в браузере Firefox при открытии новой вкладки,
                 она автоматически становится активной и на нее нет необходимости переключаться"""
-        
+
         with allure.step("[switch_to_window] Переключение на новую открытую вкладку"):
             try:
                 self.driver.switch_to.window(
@@ -57,7 +57,7 @@ class Base:
         """Функция проверяет присутствие кликабельного элемента на странице
             и делает scroll до него. Использование execute_script необходимо
             для корректной работы в двух браузерах Firefox и Chrome"""
-        
+
         with allure.step("[move_to_clickable_element] Портирование к новому кликабельному веб-элементу"):
             element = self.find_element_clickable(locator)
             self.driver.execute_script(
@@ -65,7 +65,7 @@ class Base:
             return element
 
     def save_screenshot(self, path: str) -> None:
-        """Функция сначала проверяет существует ли директория, 
+        """Функция сначала проверяет существует ли директория,
         если нет то создает ее и сохраняет скриншот в нее, с указанием даты и времени."""
 
         with allure.step("[save_screenshot] Сохранение скриншота"):
@@ -84,8 +84,8 @@ class Base:
 
             # Ожидает пока элемент станет видимым на странице
             element = WebDriverWait(self.driver, time).until(EC.visibility_of_element_located((By.XPATH, locator)),
-                                                    message=f"Can't find element by locator {locator}")
-            # Возвращает True в случаи если элемент находится в зоне видимости экрана 
+                                                             message=f"Can't find element by locator {locator}")
+            # Возвращает True в случаи если элемент находится в зоне видимости экрана
             # и False в ином случаи
             return element.is_displayed()
 
